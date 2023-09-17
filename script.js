09edd4543872ddc394db48b377bbcb5ffa03a261
Nnenna Udefi <miriamudefi@gmail.com>
2023-09-17 20:09:58 +0100

    added a lastResult variable that allows the previous calculation result to be used as a starting point for the new expression

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const display = document.getElementById('display');
 let currentInput = '';
+let lastResult = null;
 
 // This function accepts the values clicked as a parameter and returns the same to the textbox.
 const appendDisplay = (number) => {
+    // lastResult variable stores the previous calculation result
+    // and allows it to be used as starting point for the new expression
+    if (lastResult !== null) {
+        currentInput = lastResult.toString();
+        lastResult = null;
+    };
     currentInput += number;
     display.value = currentInput;
 };
 
 const clearDisplay = () => {
+    // When the 'C' button is clicked it would reset both the currentInput and the lastResult
     currentInput = '';
+    lastResult = null;
     display.value = '';
 };
 
@@ -16,8 +25,9 @@ const clearDisplay = () => {
 // eval takes a string argument and treats the content of that string
 // as if it were JavaScript code.
 const resultCalculation = () => {
-    let result = eval(currentInput);
+    const result = eval(currentInput);
     display.value = result;
+    lastResult = result;
     currentInput = '';
 };
 
